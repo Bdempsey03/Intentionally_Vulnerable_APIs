3c0e6ebb1a6358211948f3859e9201cc97b25129
GoldenButterMan <spencer-peterson@live.com>
2024-11-14 10:16:03 -0800

    Added username fields for user class

--- BrokenAuthentication/BrokenAuth/Models/users.cs
+++ BrokenAuthentication/BrokenAuth/Models/users.cs
@@ -1,7 +1,12 @@
-﻿namespace BrokenAuth.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrokenAuth.Models
 {
     public class users
     {
+        [Key]
         public int userId { get; set; }
+
+        public String username { get; } = "";
     }
 }
